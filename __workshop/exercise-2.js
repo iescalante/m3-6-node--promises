096c9b766a7bcb74ea9b21554c12d7c30f8bd28a
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-17 22:48:06 -0400

    figured out lat + long with geometry, ex2 done

--- __workshop/exercise-2.js
+++ __workshop/exercise-2.js
@@ -9,35 +9,34 @@ function getAddressPosition(address) {
         key: process.env.OPENCAGE_API_KEY,
         q: address
     };
+    return opencage.geocode(requestObj)
+    .then(data => {
 
+        if (data.status.code == 200) {
+          if (data.results.length > 0) {
+            const place = data.results[0];
+            return place.geometry;
+          }
+        } else if (data.status.code == 402) {
+          console.log('hit free-trial daily limit');
+          console.log('become a customer: https://opencagedata.com/pricing'); 
+        } else {
+          // other possible response codes:
+          // https://opencagedata.com/api#codes
+          console.log('error', data.status.message);
+        }
+      })
+      .then(data => {
+          console.log(data);
+          return data;
+      })
+      .catch(error => {
+        console.log('error', error.message);
+      });
 }
 
-getAddressPosition('Montreal');
+getAddressPosition('1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8');
 
 
 
-opencage.geocode({q: 'Montreal, Canada'}).then(data => {
-  console.log(JSON.stringify(data));
-  if (data.status.code == 200) {
-    if (data.results.length > 0) {
-      var place = data.results[0];
-      console.log(place.formatted);
-      console.log(place.geometry);
-      console.log(place.annotations.timezone.name);
-    }
-  } else if (data.status.code == 402) {
-    console.log('hit free-trial daily limit');
-    console.log('become a customer: https://opencagedata.com/pricing'); 
-  } else {
-    // other possible response codes:
-    // https://opencagedata.com/api#codes
-    console.log('error', data.status.message);
-  }
-}).catch(error => {
-  console.log('error', error.message);
-});
 
-// ... prints
-// Theresienhöhe 11, 80339 Munich, Germany
-// { lat: 48.1341651, lng: 11.5464794 }
-// Europe/Berlin
