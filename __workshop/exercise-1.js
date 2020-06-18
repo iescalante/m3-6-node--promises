41d409fc2fe122f197901b17e79c536d8f3d8e1e
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-17 21:14:07 -0400

    add request in package, done ex1, need to mod fct

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -2,9 +2,26 @@
 // -----------------------------
 
 // require the 'request-promise' module.
-
+const rp = require('request-promise');
 
 // Returns the current position of the ISS
+/*const issPosition = {
+    uri:
+}
+
 function getIssPosition() {
 
-}
+}*/
+
+var options = {
+    uri: 'http://api.open-notify.org/iss-now.json',
+    json: true // Automatically parses the JSON string in the response
+};
+ 
+rp(options)
+    .then(function (results) {
+        console.log(results);
+    })
+    .catch(function (err) {
+       console.log('OOPS!', err) // API call failed...
+    });
