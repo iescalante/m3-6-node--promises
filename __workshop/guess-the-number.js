e5568f1ffea2cfb379926187f45a3bde282f5332
iescalante <ivanenriqueescalantecornejo@gmail.com>
2020-06-20 17:21:53 -0400

    ex6 completed

--- __workshop/guess-the-number.js
+++ __workshop/guess-the-number.js
@@ -1,48 +1,40 @@
-const inquirer = require('inquirer');
-const hiddenNumber = (Math.round(Math.random()*10));
-console.log(hiddenNumber);
-let tries = 5;
-
-const output = [];
+const inquirer = require("inquirer");
+const hiddenNumber = Math.round(Math.random() * 100);
+let guesses = 0;
 
 const question = {
-    type: 'input',
-    name:'guess',
-    message: 'What is the number from 1 to 10?',
+  type: "input",
+  name: "question",
+  message: "Can you guess the number, try between 1-100?",
+  validate: function(value) {
+    let valid = !isNaN(parseFloat(value));
+    return valid || "Please enter a number";
+  },
 };
 
+function guessNumber() {
+  console.log(`You have ${5 - guesses} guesses left. Good Luck!`);
+  guesses++;
+  if (guesses < 6) {
+    inquirer.prompt(question).then((answer) => {
+      let number = answer.question;
 
-
-
-function ask() {
-    console.log(`Guess the number from 1-10. You have ${tries} tries`);
-    
-    if (tries < 6) {
-        inquirer.prompt(question).then(answer => {
-            if (answer > 10 || answer < 0) {
-                console.log('Come on, follow the rules now :)');
-                tries--;
-                guessNumber();
-            }
-            else if (answer > hiddenNumber) {
-                console.log('Too high! Try again!');
-                tries--;
-                guessNumber();
-            }
-            else if (answer < hiddenNumber) {
-                console.log('Too low! Try again!');
-                tries--;
-                guessNumber();
-
-            } else {
-                console.log('You did it! Congrats!');
-            }
-
-        })
-    } else if (tries === 0) {
-        console.log('Sorry! Out of tries :(');
-    }
+      if (number > 100 || number < 0) {
+        console.log("Whoa there, follow the rules :)");
+      } else if (number > hiddenNumber) {
+        console.log("~~~Too High! Try Again!~~~");
+        guessNumber();
+      } else if (number < hiddenNumber) {
+        console.log("~~~Too Low! Try Again!~~~~");
+        guessNumber();
+      } else {
+        console.log("*****Congratulations, you win!*****");
+      }
+    });
+  } else {
+    console.log(`-----Oh well.... Nice try, the hidden number was ${hiddenNumber}-----`);
+  }
 }
 
-ask();
+guessNumber();
 
